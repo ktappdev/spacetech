525e54ff9d9427217583a75cdd78393f9da544c8
Ken Taylor <kentaylorappdev@gmail.com>
2025-02-28 14:32:03 +0000

    update build process

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,27 @@
+// module.exports = {
+//     apps: [
+//         {
+//             name: "spacetech",
+//             script: "pnpm",
+//             args: "preview -- --port=3001 --host=0.0.0.0 --allowed-hosts=spacetech.lyricut.com",
+//             interpreter: "none",
+//             env: {
+//                 NODE_ENV: "production"
+//             }
+//         },
+//     ],
+// };
+
 module.exports = {
-    apps: [
-        {
-            name: "spacetech",
-            script: "pnpm",
-            args: "preview -- --port=3001 --host=0.0.0.0 --allowed-hosts=spacetech.lyricut.com",
-            interpreter: "none",
-            env: {
-                NODE_ENV: "production"
-            }
-        },
-    ],
+  apps: [
+    {
+      name: 'spacetech',
+      script: './dist/server/entry.mjs',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3001
+      }
+    }
+  ]
 };
+
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  preview: {
-    allowedHosts: 'spacetech.lyricut.com'
-  }
-});
